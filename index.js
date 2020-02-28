c2f8a90d9a02d20347623b6e93bac878385c0cce
Yann Xiaoyan Yang <yannxyang@sina.com>
2020-02-28 21:31:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ happyHolidaysTo('Yann')
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`;
     }
-    
+
 happyHolidaysTo("Labor Day",'Yann')
 function happyCustomHolidayTo(holiday,name){
   return `Happy ${holidays}, ${name}!`;
