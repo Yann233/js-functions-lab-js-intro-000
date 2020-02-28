2f3b966a328a3cc039ac4954628081e2a7693b71
Yann Xiaoyan Yang <yannxyang@sina.com>
2020-02-28 21:29:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-//Write a function named happyHolidays. This function should not accept any parameters and should return the string "Happy holidays!".
-function happyHolidays(){
-  return "Happy holidays!";
-}
-//Write a function named happyHolidaysTo. This function should accept a parameter of the name of the person you want to wish happy holidays, and return the string `Happy holidays, ${name}!`
+function happyHolidays() {
+    return "Happy holidays!";}
+
+
 happyHolidaysTo('Yann')
-function happyHolidaysTo(name){
+function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`;
-}
-//Write a function named happyCustomHolidayTo. This function should accept two parameters, the holiday you want to wish them well for, and the name of the person you're wishing well. Order of parameters matters, so make sure to first pass in the holiday and then the name. This function should return the string `Happy ${holiday}, ${name}!`
-function happyCustomHolidayTo(holiday,name){
-  return `Happy ${holiday}`,${name}!`;
-}
+    }
